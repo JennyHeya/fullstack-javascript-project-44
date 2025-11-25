ff42fe8028bf31bbc7cf476a57cbbdb989afa179
Jenny <lisalthe@gmail.com>
2025-11-25 03:11:04 +0300

    feat: brain-even game with engine and utils

---
+++ bin/brain-even.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import runGame from '../src/engine.js';
+import getRandomInt from '../src/utils.js';
+
+const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (num) => num % 2 === 0;
+
+const generateRound = () => {
+  const question = getRandomInt(1, 100);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+  return { question, correctAnswer };
+};
+
+runGame(DESCRIPTION, generateRound);
